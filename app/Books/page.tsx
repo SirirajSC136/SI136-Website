c967161d6527f242bc4b2e517e8e1bbd57e1fb73
ArunS0042 <arun.ss2006@gmail.com>
2025-11-04 11:40:25 +0700

    Update page.tsx

--- app/Books/page.tsx
+++ app/Books/page.tsx
@@ -181,7 +181,11 @@ export default async function BookPage() {
 							Please log in to access the book library
 						</h2>
 						<p className="text-secondary mb-6">
-							Sign in with the account (@student.mahidol.edu) to accesss the textbooks.
+							Sign in with the account{" "}
+							<strong>
+								<u>@student.mahidol.edu</u>
+							</strong>{" "}
+							to accesss the textbooks.
 						</p>
 						<LoginButton />
 					</div>
